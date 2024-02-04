35204e7685dbacba1208ae1f5cf0d99a9a5e78a8
wampiros <107957465+wampiros@users.noreply.github.com>
2024-02-04 20:01:19 +0100

    Create gonciarz.py

---
+++ gonciarz.py
@@ -0,0 +1,40 @@
+print("Krzysztof Jan Gonciarz (ur. 19 czerwca 1985 w Krakowie[4]) – polski twórca internetowy. Autor kanałów wideoblogowych Zapytaj Beczkę oraz Krzysztof Gonciarz w serwisie YouTube, współtwórca (wraz z Katarzyną Męciński[5][6]) kanału TheUwagaPies. Były dziennikarz portalu Gry-Online[7].")
+
+print("Życiorys")
+print("Edukacja i kariera")
+print("Absolwent II Liceum Ogólnokształcącego im. Króla Jana III Sobieskiego w Krakowie oraz kulturoznawstwa na Uniwersytecie Jagiellońskim[8]. Jego pierwsze sukcesy zawodowe to m.in. udział w tworzeniu oficjalnego spolszczenia gry Baldur’s Gate 2 czy praca w internetowej telewizji tvgry.pl[8]. Pracę dla Gry-Online zakończył pod koniec 2012 roku[9]. Dubbingował postać „Stuart Zurg” w grze Ratchet & Clank: Załoga Q[10].")
+
+print("Na początku 2016 roku nawiązał współpracę reklamową z firmą Intel promującą procesory tej firmy. W ramach projektu pt. Sztuka Składania Historii nagrał 4 filmy w kilku wybranych miejscach w Azji – „Więcej niż jedno życie” (Hongkong), „Gdybyś był Japończykiem” (Tokio), „48 godzin wspomnień” (Seul) i „Krok pierwszy” (Tajwan). Oprócz tego zostały nagrane także materiały „making of” z serii, w których opowiadał o tworzeniu scenariuszy do filmów, a także udzielał rad na temat montażu filmów[11].")
+
+print("W 2016 roku założył konto na portalu crowdfundingowym Patronite[12]. Przez pewien czas był tam najchętniej wspieranym twórcą (ponad 20 tys. zł miesięcznie). Zebrane środki przeznaczył na realizację minifilmu dokumentalnego o japońskim lesie samobójców Aokigahara. Na początku 2021 roku profil zbierał 1650 złotych miesięcznie i plasował się w trzeciej setce rankingu według kwoty wspierających[13].")
+
+print("W lutym 2018 roku we współpracy ze stacją telewizyjną Eurosport w trakcie zimowych igrzysk olimpijskich w Pyeongchangu stworzył serie filmów, pokazujących kulisy tego wydarzenia sportowego[14]. Współpraca ta miała bardzo dobry odbiór, dlatego też została już zapowiedziana seria filmów z kolejnych, tym razem letnich igrzysk olimpijskich w Tokio[15].")
+
+print("12 lipca 2019 odbył się wernisaż wystawy jego autorstwa w Muzeum Sztuki i Techniki Japońskiej „Manggha” w Krakowie zatytułowanej „Tokio 24”[16]. Na wystawę składały się 20 minutowy film ukazujący cykl dobowy w Tokio, sala „mikrozachwytów” oraz sala neonów[17]. Wystawa była dostępna w muzeum do 29 września 2019 roku[18] i stała się najpopularniejszą ekspozycją w jego dotychczasowej historii[19].")
+
+print("Życie prywatne")
+print("Od 2014 roku mieszka w Japonii, w tokijskiej dzielnicy Shibuya[20]. W pierwszym roku pobytu w tym kraju założył przedsiębiorstwo o nazwie Tofu Media, działające w branży filmowej. W 2018 roku założono jego polski oddział[21]. W 2023 roku wyznał, że jest biseksualny[22]")
+
+print("Amatorsko uprawia bieganie. Wziął udział w czterech maratonach – dwukrotnie w londyńskim (2018 i 2019), a także raz w nowojorskim (2017) i warszawskim (2018). 6 maja 2018 roku, Gonciarz uczestniczył w charytatywnym biegu Wings for Life World Run, gdzie przebiegł 19,4 km[23][24].")
+
+print("Starty Krzysztofa Gonciarza w biegach maratońskich")
+print("Data	Miejsce	Czas	Pozycja	Nr startowy")
+print("5 listopada 2017	Stany Zjednoczone Nowy Jork	3:56:07[25]	12 199	38338")
+print("22 kwietnia 2018	Wielka Brytania Londyn	4:11:38[26]	8868	55295")
+print("30 września 2018	Polska Warszawa	4:42:17[27]	5006	5555")
+print("28 kwietnia 2019	Wielka Brytania Londyn	3:53:42[28]	9112	58378")
+print("Kontrowersje")
+print("We wrześniu 2023 roku trzy kobiety oskarżyły Gonciarza o zmuszanie ich do przyjmowania narkotyków oraz przemocowe i seksistowskie traktowanie[29][30], niedługo potem podobne oskarżenia wysunęła także modelka Hanna Koczewska[31]. Jego była partnerka Kasia Mecinski potwierdziła, że Krzysztof ma długą historię przemocowego zachowania[32]. W odpowiedzi twórca opublikował oświadczenie, w którym przeprosił za swoje zachowanie i przekonywał, że sam „nie rozumiał własnego zachowania” oraz że poddaje się terapii[33][31][34].")
+
+print("W listopadzie 2023 roku opublikował na portalu YouTube film, w którym oskarżał jedną z kobiet, które wysunęły przeciwko niemu oskarżenia, o stalking. Poinformował także, że ma zamiar złożyć zawiadomienie o możliwości popełnienia przez nią przestępstwa[35][36].")
+
+print("Publikacje książkowe")
+print("Wybuchające beczki – zrozumieć gry wideo (2011)[37]")
+print("U Mad? The Internet’s Guide to Idiots (2012)[38]")
+print("WebShows: Sekrety wideo w Internecie (2012)[39][40]")
+print("Rozum i godność człowieka (2019)[41]")
+print("Nagrody i wyróżnienia")
+print("Traveler 2015 w kategorii „Podróżnik Online” przyznana przez polską edycję magazynu National Geographic Traveler (2016)[42].")
+print("Dwie nagrody Grand Video Awards 2016: w kategorii Wideotutorial za film „Jak zostać youtuberem [Sztuka Składania Historii]”, oraz w kategorii Lifestyle za film „Więcej niż JEDNO ŻYCIE” (2016)[43]")
+print("Nagroda Smart TV Award oraz Audio Network przyznana przez Samsunga (2016)[44].")
+print("Nagroda Specjalna „Korony Równości” przyznana przez Kampanię Przeciw Homofobii za zaangażowanie w walkę o równe prawa dla społeczności LGBTQI (2020)[45][46][47]")
